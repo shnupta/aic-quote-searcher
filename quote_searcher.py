f401226827d5aa66c28a0ad9c2d52529d49a5476
Casey Williams <williamshoops96@gmail.com>
2017-04-08 21:15:58 +0100

    made searches not dependent on case

--- quote_searcher.py
+++ quote_searcher.py
@@ -29,22 +29,26 @@ while (True):
 	if(quote == "%quit%"):
 		print("Bye!")
 		exit()
-	padded_quote = str(" " + quote + " ")
+	if(quote == ""):
+		continue
+	#padded_quote = str(" " + quote + " ")
 	print("")
 	with open(filename, 'r') as f:
 		result = 0
 		results = []
 		for (i, line) in enumerate(f):
-			if (padded_quote in line): 
+			if (quote in line.lower()): 
 				lines_per_page = 34
 				act = find_act(i, filename)
 				person = line.split(":")[0].replace("\t", "")
 				print("("+ str(result) +") The quote '" + quote + "' is roughly on page " + str((i/lines_per_page)+1) +  " spoken by " + person + " in " + act)
 				result += 1
 				results.append(line)
+	
 	if(len(results) == 0):
 		print("Not found\n")
-
+		continue	
+	
 	while(True):
 		print("")
 		choice = raw_input("If you'd like to see the full line, enter the result number in the brackets, else just press enter: ")
